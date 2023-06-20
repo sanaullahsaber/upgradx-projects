de93cd463ee493fb4b03bd25a8bcc7d7c54ccadc
sanaullahsabere <you@example.com>
2023-06-20 17:07:53 +0600

    submit

--- src/components/ZetEffect/ZetEffect.js
+++ src/components/ZetEffect/ZetEffect.js
@@ -1,11 +1,20 @@
 'use client'
-import React from 'react';
+import React, { useRef, useState } from "react";
+
 import './ZetEffect.css'
 import zetImg from "../../../public/peeps1.png"
 import Image from 'next/image';
 
 
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
 
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
+import { Autoplay, Pagination, Navigation } from "swiper";
 
 const ZetEffect = () => {
  
@@ -24,12 +33,41 @@ const ZetEffect = () => {
               decoding="async"
               data-nimg="1"
               style="color:transparent"
-              className="sc-eKCZMz cmROcX"
+              className="sc-eKCZMz cmROcX hidden md:block lg:block xl:block"
             ></Image>
-            <div className="hufdxl">
-              <p className="gHPgIl">THE ZET EFFECT</p>
+            <div className="hufdxl mb-10">
+              <p className="gHPgIl ml-28">THE ZET EFFECT</p>
               {/*  */}
-          
+              <Swiper
+                spaceBetween={30}
+                centeredSlides={true}
+                autoplay={{
+                  delay: 2500,
+                  disableOnInteraction: false,
+                }}
+               
+                modules={[Autoplay,  Navigation]}
+                className="mySwiper"
+              >
+                <SwiperSlide>
+                  <p className="md:text-3xl md:mr-28 text-base leading-10 font-extrabold text-white mb-w ml-10">
+                    14 Lakh+ Financial Advisor across India are using ZET to
+                    increase their income.
+                  </p>
+                </SwiperSlide>
+                <SwiperSlide>
+                  <p className="md:text-3xl text-base leading-10 font-extrabold text-white mb-w ml-10">
+                    We helped customers in more than 50 cities to get their
+                    first financial product
+                  </p>
+                </SwiperSlide>
+                <SwiperSlide>
+                  <p className="md:text-3xl text-base leading-10 font-extrabold text-white mb-w ml-10">
+                    More than ₹20 Cr earned by our Agents by selling Credit
+                    Cards and Loans
+                  </p>
+                </SwiperSlide>
+              </Swiper>
             </div>
           </div>
         </div>
